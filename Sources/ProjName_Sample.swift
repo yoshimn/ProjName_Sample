386e120ceaeb3242d933f2e9fe2a67cbacef67c5
yoshimn <yoshigithub@gmail.com>
2017-09-11 16:36:15 +0900

    public

--- Sources/ProjName_Sample.swift
+++ Sources/ProjName_Sample.swift
@@ -1,4 +1,17 @@
-struct ProjName_Sample {
+public struct ProjName_Sample {
 
-    var text = "Hello, World!"
+    public var text = "Hello, World!"
+}
+
+public func f() {
+    
+    print("Hello, World!!")
+    
+}
+
+public enum E {
+    
+    case a
+    case b
+    
 }
